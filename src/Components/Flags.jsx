3f801961fa4c01705204c0fe1b50aea98f0b217a
jpickler <sirzymok@gmail.com>
2023-12-10 17:52:26 -0500

    added  functionality to the html

--- src/Components/Flags.jsx
+++ src/Components/Flags.jsx
@@ -16,10 +16,16 @@ const Flags = () => {
   const [button3IdName, setButton3IdName] = useState("wrong");
   const [button4IdName, setButton4IdName] = useState("wrong");
   const [theFlagImage, setTheFlagImage] = useState(``);
+  const [correctAnswer, setCorrectAnswer] = useState(``);
+  const [playerScore, setPlayerScore] = useState(0);
+  const [refreshTheBoard, setRefreshTheBoard] = useState(false);
+  const [round, setRound] = useState(0);
+  const [gameStatus, setGameStatus] = useState(`asking`);
 
   //Variables to adjust dificulty and expand with more flags:
   const numFlags = 50;
   const numberOfAnswers = 4; // later can add more buttons
+  let numberOfRounds = 5;
 
 
   // const getRandomFlag = () => {
@@ -35,14 +41,16 @@ const Flags = () => {
     let randomButton = null;
     let indexOfNumToDelete = null;
     let randomChoice = null;
-
+    setRound(round+1);
     const assignButton = async () => {
+    
 
       for (let i = 1; i < 5; i++) {
         if (isAnswer) {
           //do this for the first iteration.. it is the answer
           const randomAnswer = (Math.floor(Math.random() * possibleFlags.length)) + 1;
           setTheFlagImage(flagData[randomAnswer].flagImage);
+          setCorrectAnswer(flagData[randomAnswer].name);
           buttonName = flagData[randomAnswer].name;
           //remove the value from the array of choices
           indexOfNumToDelete = possibleFlags.indexOf(randomAnswer); 
@@ -66,16 +74,16 @@ const Flags = () => {
         //set the button name
         switch (buttonChoice) {
           case 1: await setButton1Name(buttonName);
-            if (isAnswer) {setButton1IdName(`answer`)};
+            if (isAnswer) {setButton1IdName(buttonName)};
             break;
           case 2: await setButton2Name(buttonName);
-            if (isAnswer) {setButton2IdName(`answer`)};
+            if (isAnswer) {setButton2IdName(buttonName)};
             break;
           case 3: await setButton3Name(buttonName);
-          if (isAnswer) {setButton3IdName(`answer`)};
+          if (isAnswer) {setButton3IdName(buttonName)};
             break;
           case 4: await setButton4Name(buttonName);
-            if (isAnswer) {setButton4IdName(`answer`)};
+            if (isAnswer) {setButton4IdName(buttonName)};
             break;
         };
 
@@ -94,33 +102,84 @@ const Flags = () => {
 
     useEffect(() => {
       setTheQuestion();
-    }, []);
+    }, [refreshTheBoard]);
+
+    const resetTheBoard = () => {
+      if (round === numberOfRounds){
+        //end the game
+        setGameStatus(`ending`)
+        //show score
+        // ask to play again
+
+
+      }else{
+      setRefreshTheBoard(!refreshTheBoard);
+
+      }
+
+    };
+
 
     const handleButtonClick = (id) => {
       console.log(`button was clicked id =`, id);
-    };
+      console.log(`correctAnswer: `, correctAnswer);
+      console.log(`id: `, id);
+      if(id===correctAnswer){
 
+        setPlayerScore(playerScore+1);
+        //alert(`That is correct!`);
+      }else{
+        alert(`Sorry, that is incorrect. The correct answer is: ${correctAnswer}` );
+      };
+
+      resetTheBoard();
+
+    };
+    const startTheGame =() =>{
+      setGameStatus(`playing`);
+      resetTheBoard();
+    }
+    const homeScreen = () => {
+      setGameStatus(`asking`);
+      setPlayerScore(0);
+      setRefreshTheBoard(false);
+      setRound(0);
+    }
 
 
 
   return (
 
-    // <>
-    //   {loading ? (
-    //     <p>Loading...</p>
-    //   ) : (
     <>
+    {gameStatus === "asking" && (
+      <>
+        <h2>Would you like to play the flag game?</h2>
+        <button id="start" onClick={()=>(startTheGame())}>Press To Play</button>
+      </>
+    )}
 
+    {gameStatus === "playing" && (
+      <>
+      <h2> The current round is: {round}</h2>
+      <h2> Your Score is: {playerScore}</h2>
       <h2> Which State flag is this?</h2>
       <img id="flagImage" src={theFlagImage} alt="picture of a state flag" />
         <button id = {button1IdName} onClick={() => handleButtonClick(button1IdName)}> {button1Name} </button>
         <button id = {button2IdName} onClick={() => handleButtonClick(button2IdName)}> {button2Name} </button>
         <button id = {button3IdName} onClick={() => handleButtonClick(button3IdName)}> {button3Name} </button>
         <button id = {button4IdName} onClick={() => handleButtonClick(button4IdName)}> {button4Name} </button>
+        </>
+    )}
 
-    </>
-    // )}
-    // </>
+    {gameStatus === "ending" && (
+      <>
+        <h2>Your score is: {playerScore} out of {numberOfRounds} rounds</h2>
+        <h2> Would you like to play again?</h2>
+        <button id="start" onClick={()=>(startTheGame())}>Press To Play</button>
+        <button id="done" onClick={()=>(homeScreen())}>I'm done playing</button>
+      </>
+    )}
+  </>
   );
 };
 
