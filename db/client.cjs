ac46ee4409cdeddc5826ff5a2c82125242c94af4
jpickler <sirzymok@gmail.com>
2023-12-26 10:44:40 -0500

    updated client.cjs

--- db/client.cjs
+++ db/client.cjs
@@ -1,4 +1,15 @@
-const {Client} = require(`pg`);
-const client = new Client(process.env.DATABASE_URL);
+// const {Client} = require(`pg`);
+// const client = new Client(process.env.DATABASE_URL);
 
-module.exports = client;
+// module.exports = client;
+
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'u754198335_twobitbandit',
+  host: 'localhost',
+  database: 'u754198335_twobit_db',
+  password: 'Dragon88**',
+  port: 5432, // Default PostgreSQL port
+});
